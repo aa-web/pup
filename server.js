a82b9a4c2f5c37d60a39014efc61bc3b90dbf769
aa-web <ghanielyagoubi@gmail.com>
2024-06-20 17:46:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // Set up Puppeteer options for running in Render.com
 const renderCloudConfig = {
